af4c50e4980ded6adf418e9bd79f72f506cd58ca
ByungDae Sohn <miconblog@gmail.com>
2019-01-29 16:05:49 +0900

    Todos 컴포넌트

---
+++ projects/Todos.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Todos = (props) => {
+
+  console.log('----->', props);
+
+  return (
+    <div className="Todos" style={{backgroundColor: '#ccc'}}>
+      <h2>{props.title}</h2>
+      <ul>
+        {props.items.map((item, index) => {
+          return <li key={index}><input type="checkbox"/>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export function x() {
+  return 1;
+}
+
+export default Todos;
